99743cd78fa4c9d0d89dd347734bdcbd8efedf37
Namco Four <namco-4@gmx.de>
2015-10-06 23:42:29 +0200

    First implementation of the Shader class

--- nForgeEngine/main.cpp
+++ nForgeEngine/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <GL\glew.h>
 #include "Display.h"
+#include "Shader.h"
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char** argv) {
 	Display display(1280, 720, "Hello World!");
+
+	Shader shader("./res/basicShader");
 
 	while (!display.isClosed()) {
 		display.Clear(0.33f, 0.33f, 0.66f, 1.0f);
+		shader.Bind();
+
 		display.Update();
 	}
 
