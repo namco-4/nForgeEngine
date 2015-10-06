465fe9c3fe93a62ab316dc16f017a7de6ac0ed60
Namco Four <namco-4@gmx.de>
2015-10-07 01:09:11 +0200

    First implementation of the Mesh class

--- nForgeEngine/main.cpp
+++ nForgeEngine/main.cpp
@@ -2,17 +2,23 @@
 #include <GL\glew.h>
 #include "Display.h"
 #include "Shader.h"
+#include "Mesh.h"
 
 using namespace std;
 
 int main(int argc, char** argv) {
 	Display display(1280, 720, "Hello World!");
-
+	Vertex vertices[] = {	Vertex(glm::vec3(-0.5f, -0.5f, 0)),
+							Vertex(glm::vec3(0, 0.5f, 0)),
+							Vertex(glm::vec3(0.5f, -0.5f, 0))};
+	Mesh mesh(vertices, sizeof(vertices)/sizeof(vertices[0]));
 	Shader shader("./res/basicShader");
 
 	while (!display.isClosed()) {
-		display.Clear(0.33f, 0.33f, 0.66f, 1.0f);
+		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
+
 		shader.Bind();
+		mesh.Draw();
 
 		display.Update();
 	}
