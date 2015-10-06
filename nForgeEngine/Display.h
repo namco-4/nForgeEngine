d23f24b9b516472b859284f79346e1402b85d869
Namco Four <namco-4@gmx.de>
2015-10-06 21:55:22 +0200

    4th commit

--- nForgeEngine/Display.h
+++ nForgeEngine/Display.h
@@ -1,8 +1,23 @@
 #pragma once
-class Display
-{
+
+#include <string>
+#include <SDL2\SDL.h>
+
+class Display {
+
 public:
-	Display();
-	~Display();
+	Display(int, int, const std::string&);
+	virtual ~Display();
+	void Update();
+	bool isClosed();
+	void Clear(float, float, float, float);
+protected:
+private:
+	Display(const Display& other) {}
+	Display& operator=(const Display& other) {}
+
+	SDL_Window* m_window;
+	SDL_GLContext m_glContext;
+	bool m_isClosed;
 };
 
--- nForgeEngine/main.cpp
+++ nForgeEngine/main.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
+#include <GL\glew.h>
 #include "Display.h"
 
-int main(int argc, char** argv) {
-	Display display();
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	Display display(1280, 720, "Hello World!");
+
+	while (!display.isClosed()) {
+		display.Clear(0.33f, 0.33f, 0.66f, 1.0f);
+		display.Update();
+	}
+
 	return 0;
 }
